9b1f4424827b12266bd9bb27f256278178d39c0c
Tigran <tigran19910315@gmail.com>
2023-08-24 18:27:50 +0400

    first commit

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import styles from './page.module.css'
+
+const Portfolio = () => {
+  return (
+    <div className={styles.container}>
+    <h1 className={styles.selectTitle}>Choose aaaa gallery</h1>
+    <div className={styles.items}>
+      <Link href="/portfolio/illustrations" className={styles.item}>
+        <span className={styles.title}>Illustrations</span>
+      </Link>
+      <Link href="/portfolio/websites" className={styles.item}>
+        <span className={styles.title}>Websites</span>
+      </Link>
+      <Link href="/portfolio/applications" className={styles.item}>
+        <span className={styles.title}>Application</span>
+      </Link>
+    </div>
+  </div>
+  )
+}
+
+export default Portfolio
